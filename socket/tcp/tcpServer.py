b2a3bf6d71610984c8a3ba69be64d74bb3f4f95f
yong <mail@yongtang.tech>
2021-10-22 10:55:22 -0400

    modified to threading

--- socket/tcp/tcpServer.py
+++ socket/tcp/tcpServer.py
@@ -1,22 +1,31 @@
 #! /usr/bin/python3
 
 import socket
+import threading
 
-server = socket.socket()
-ip_port = ('172.16.157.135', 9999)
-server.bind(ip_port)
-server.listen()
-server.accept()
-s1, ip = server.accept()
+IP = '0.0.0.0'
+PORT = 6666
 
-while True:
-    data = s1.recv(1024)
-    print(data.decode())
-    s1.send('ack.{}'.format(data.decode()).encode())
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen(2)
+    print(f'[*] Listening on {IP}:{PORT}')
 
-s1.close()
+    while True:
+        client, address = server.accept()
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_handler.start()
 
-server.close()
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
 
 
 
